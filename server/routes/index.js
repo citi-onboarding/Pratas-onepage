c3780e9af1e52082b10c88838c9f48d4b0aa5dd3
isaquetdiniz <itd@cin.ufpe.br>
2020-07-06 10:52:01 -0300

    Created Routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,11 @@ const keystone = require('keystone');
 const cors = require('cors');
 
 const Example = keystone.list('Examples');
+const aboutUs = keystone.list('Quem Somos');
+const banner = keystone.list('Banner');
+const footer = keystone.list('Social');
+const products = keystone.list('Produtos');
+const testimonies = keystone.list('Testimonies');
 
 module.exports = (app) => {
   app.use(cors());
@@ -17,4 +22,40 @@ module.exports = (app) => {
       res.send(items);
     });
   });
+
+  app.get('/api/aboutUs', (req, res) => {
+    aboutUs.model.find((err, items) => {
+      if (err) return res.apiError('database error', err);
+      res.send(items);
+    });
+  });
+
+  app.get('/api/banner', (req, res) => {
+    banner.model.find((err, items) => {
+      if (err) return res.apiError('database error', err);
+      res.send(items);
+    });
+  });
+
+  app.get('/api/footer', (req, res) => {
+    footer.model.find((err, items) => {
+      if (err) return res.apiError('database error', err);
+      res.send(items);
+    });
+  });
+
+  app.get('/api/products', (req, res) => {
+    products.model.find((err, items) => {
+      if (err) return res.apiError('database error', err);
+      res.send(items);
+    });
+  });
+
+  app.get('/api/testimonies', (req, res) => {
+    testimonies.model.find((err, items) => {
+      if (err) return res.apiError('database error', err);
+      res.send(items);
+    });
+  });
+  
 };
