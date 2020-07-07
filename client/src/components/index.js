6ca6f1b412dbda92ccd36d1d39d3a552e46088e0
isaquetdiniz <itd@cin.ufpe.br>
2020-07-07 11:57:50 -0300

    Created Contact/

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,10 +2,12 @@ import Banner from './Banner';
 import AboutUs from './AboutUs';
 import Contact from './Contact';
 import Footer from './Footer';
+import Contact from './Contact';
 
 export {
   Banner,
   AboutUs,
   Contact,
   Footer,
+  Contact,
 };
