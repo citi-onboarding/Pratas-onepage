c2d02af00d9bc8c1624affb610a8f7afc3e96d15
isaquetdiniz <itd@cin.ufpe.br>
2020-07-07 12:37:04 -0300

    base contact session

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -4,7 +4,20 @@ import './Contact.css';
 
 function Contact(){
     return(
-        <h1>Contact Session</h1>
+        <section className="contact-session">
+            <div className="contact-session-form">
+                <h1>Contato</h1>
+                <form action="" target="_blank">
+                    <input type="text" placeholder="Nome"/>
+                    <input type="text" placeholder="Telefone"/><br/>
+                    <input type="text" placeholder="Email"/><br/>
+                    <input type="text" placeholder="Como nos conheceu?"/><br/>
+                    <input type="textarea" placeholder="Mensagem"/><br/>
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
+            <img src="" alt="Contato Imagem"/>
+        </section>
     );
 }
 
