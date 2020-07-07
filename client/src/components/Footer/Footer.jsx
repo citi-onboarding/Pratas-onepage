96188752f6ecd586aadcc5a3458f1151c16914ea
isaquetdiniz <itd@cin.ufpe.br>
2020-07-07 11:49:15 -0300

    Contact/ rename for Footer/

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,40 +1,33 @@
 import React from "react";
-import logoFooter from './logo-footer.png';
-import iconFacebook from './icon-facebook.png';
-import iconInstagram from './icon-instagram.png';
-import iconPortal from './icon-portal.png'
 
 import './Footer.css';
 
 function Footer(){
     return(
-        <footer>
-            <section className="footer-session-main">
-                    <img src={logoFooter} alt="Jacke Menck Pratas footer" className="logoFooter"/>
+        <footer className="footer-session">
+            <img src="" alt="Jacke Menck Pratas footer"/>
+            <section className="footer-session-social">
                 <div>
-                    <h3>CONTATO</h3>
-                    <a href="http://" target="_blank" rel="noopener noreferrer" className="sup">(87) 99634-2494</a>
+                    <img src="" alt="Instagram" className="footer-img"/>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">Instagram<br></br>/@jacquemenckpratas</a>
+                    <img src="" alt="Email" className="footer-img"/>
                     <a href="http://" target="_blank" rel="noopener noreferrer">jacqueline.guerra@hotmail.com</a>
                 </div>
                 <div>
-                    <h3>SIGA-NOS</h3>
-                    <div className="icons-footer">
-                        <a href="http://" target="_blank" rel="noopener noreferrer" className="sup">
-                            <img src={iconFacebook} alt="Facebook" className="icon-footer"/>
-                        </a>
-                    
-                        <a href="http://" target="_blank" rel="noopener noreferrer" className="sup">
-                            <img src={iconInstagram} alt="Instagram" className="icon-footer"/>
-                        </a>
-                    </div>
-                    <a href="http://" target="_blank" rel="noopener noreferrer">
-                    <img src={iconPortal} alt="Portal Administrador" className="icon-portal"/>
-                    </a> 
+                    <img src="" alt="Facebook" className="footer"/>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">Facebook<br></br>/@jacquemenckpratas</a>
+                    <img src="" alt="Telefone" className="footer-img"/>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">(87) 99634-2494</a>
                 </div>
             </section>
-            <section className="footer-session-secondary">
-                    <p>&copy; 2020 CITi - Todos os direitos reservados</p>
-                    <p>Made with &lt;&frasl;&gt; and &hearts;</p>
+            <section className="footer-session-end">
+                <div className="footer-session-adm">
+                    <img src="" alt="Portal Administrador"/>
+                    <p>Portal do Adminitrador</p>
+                </div>
+                <div className="footer-session-citi">
+                    <p>Made with &lt;&frasl;&gt; and &lt;3 by CITi</p>
+                </div>
             </section>
         </footer>
     );
