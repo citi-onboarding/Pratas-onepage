1f23a6939ec164a364f37ed8e7dfec9af3b8ebc9
isaquetdiniz <itd@cin.ufpe.br>
2020-07-08 19:32:31 -0300

    Finished Contact Session for PC

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import contactImage from './contact-image.png'
 import './Contact.css';
 
 function Contact(){
@@ -8,15 +9,17 @@ function Contact(){
             <div className="contact-session-form">
                 <h1>Contato</h1>
                 <form action="" target="_blank">
-                    <input type="text" placeholder="Nome"/>
-                    <input type="text" placeholder="Telefone"/><br/>
+                    <input type="text" placeholder="Nome" className="first" id="first"/>
+                    <input type="text" placeholder="Telefone" className="first"/><br/>
                     <input type="text" placeholder="Email"/><br/>
                     <input type="text" placeholder="Como nos conheceu?"/><br/>
-                    <input type="textarea" placeholder="Mensagem"/><br/>
-                    <button type="submit">Enviar</button>
+                    <input type="textarea" placeholder="Mensagem" className="message"/><br/>
+                    <input type="submit" className="submit"/>
                 </form>
+            </div> 
+            <div className="contact-session-image">
+                <img src={contactImage} alt="Contact Session Image"/> 
             </div>
-            <img src="" alt="Contato Imagem"/>
         </section>
     );
 }
