9fc8fa5d53cb494cf9aab0be3b15f6f394d700d7
isaquetdiniz <itd@cin.ufpe.br>
2020-07-09 22:28:57 -0300

    Slider Bug not resolved

--- client/src/components/Testimonials/Testimonials.jsx
+++ client/src/components/Testimonials/Testimonials.jsx
@@ -10,16 +10,15 @@ import imgAspasE from './img-aspasE.png';
 
 
 function Testimonials() {
-/*
+
      const settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1,
-        adaptiveHeight: true
-     };
-*/
+        slidesToScroll: 1
+     }
+
     return(
         <section className="testimonials-session">
             <div className="testimonials-session-title">
@@ -29,19 +28,21 @@ function Testimonials() {
                 </div>
             </div>
             <div className="testimonials-session-carrossel">
-                <div className="carrossel-item">
-                    <img src={imgTestimonials} alt="Testimonials Imagem" className="carrossel-image"/>
-                    <div className="carrossel-text">
-                        <h4>Josimeire Pinheiro</h4>
-                        <p><img src={imgAspasE} alt="Aspas Esquerda"/><br/>Eu amo Prata! Gosto de usar muitas coisas ao mesmo tempo. 
+                <Slider {...settings}>
+                    <div className="carrossel-item">
+                        <img src={imgTestimonials} alt="Testimonials Imagem" className="carrossel-image"/>
+                        <div className="carrossel-text">
+                            <h4>Josimeire Pinheiro</h4>
+                            <p><img src={imgAspasE} alt="Aspas Esquerda"/><br/>Eu amo Prata! Gosto de usar muitas coisas ao mesmo tempo. 
                             Eu não uso apenas acessórios, e sim alegorias e adereços... rsrsrs. 
                             Quem me conhece sabe disso! E depois que conheci Jaqueline (Jack Menck Pratas)
                             , há mais de 12 anos, tudo aumentou! Muitos anéis, muito tudo. Pois ela tem produtos
                              exclusivos de qualidade e bom preço. Tudo de excelência. Sempre disponível e a disposição 
                              para agradar as clientes. Hoje é minha amiga. Mora no coração. <img src={imgAspasD} alt="Aspas Direita"/></p>
-                        <h5>- Juazeiro (Bahia)</h5>    
+                            <h5>- Juazeiro (Bahia)</h5>    
+                        </div>
                     </div>
-                </div>
+                </Slider>
             </div>
         </section>
     );
