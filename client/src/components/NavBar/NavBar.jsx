ef301ada4bc7c5dfa0011cc78d3c8c2ab4470d88
isaquetdiniz <itd@cin.ufpe.br>
2020-07-11 14:05:39 -0300

    Basic NavBar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 
+import logoNavBar from './logo-navbar.png';
 import './NavBar.css';
+
 
 function NavBar(){
     return(
-        <h1>NavBar</h1>
+    <section>
+        <header className="nav-bar">
+            <img src={logoNavBar} alt="Logo NavBar"/>
+            <nav>
+                <ul className="nav-bar-menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Produtos</a></li>
+                    <li><a href="#">Sobre Nós</a></li>
+                    <li><a href="#">Depoimentos</a></li>
+                    <li><a href="#">Contatos</a></li>
+                </ul>
+            </nav>
+        </header>
+        <p>
+            Teste <br/>
+            Teste <br/>
+            Teste <br/>
+            Teste <br/>
+        </p>
+        </section>
     );
 };
 
