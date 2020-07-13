434409fb699a25dd7ec414599e1dd19c49a2aea0
isaquetdiniz <itd@cin.ufpe.br>
2020-07-13 13:57:52 -0300

    Back to the right Branch

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,12 +2,10 @@ import Banner from './Banner';
 import AboutUs from './AboutUs';
 import Contact from './Contact';
 import Footer from './Footer';
-import Contact from './Contact';
 
 export {
   Banner,
   AboutUs,
   Contact,
   Footer,
-  Contact,
 };
