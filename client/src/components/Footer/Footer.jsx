9c1af5b5d6406ec1d9f98c065b66f810a4c0f1b5
isaquetdiniz <itd@cin.ufpe.br>
2020-07-15 10:21:47 -0300

    Add Footer/

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import logoFooter from './logo-footer.png';
+import iconFacebook from './icon-facebook.png';
+import iconInstagram from './icon-instagram.png';
+import iconPortal from './icon-portal.png'
+
+import './Footer.css';
+
+function Footer(){
+    return(
+        <footer>
+            <section className="footer-session-main">
+                    <img src={logoFooter} alt="Jacke Menck Pratas footer" className="logoFooter"/>
+                <div>
+                    <h3>CONTATO</h3>
+                    <a href="http://" target="_blank" rel="noopener noreferrer" className="sup">(87) 99634-2494</a>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">jacqueline.guerra@hotmail.com</a>
+                </div>
+                <div>
+                    <h3>SIGA-NOS</h3>
+                    <div className="icons-footer">
+                        <a href="http://" target="_blank" rel="noopener noreferrer" className="sup">
+                            <img src={iconFacebook} alt="Facebook" className="icon-footer"/>
+                        </a>
+                    
+                        <a href="http://" target="_blank" rel="noopener noreferrer" className="sup">
+                            <img src={iconInstagram} alt="Instagram" className="icon-footer"/>
+                        </a>
+                    </div>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                    <img src={iconPortal} alt="Portal Administrador" className="icon-portal"/>
+                    </a> 
+                </div>
+            </section>
+            <section className="footer-session-secondary">
+                    <p>&copy; 2020 CITi - Todos os direitos reservados</p>
+                    <p>Made with &lt;&frasl;&gt; and &hearts;</p>
+            </section>
+        </footer>
+    );
+};
+
+export default Footer;
