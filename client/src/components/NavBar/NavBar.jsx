7ca9013231b4c9e1e6998a52825c2e3858f16121
isaquetdiniz <itd@cin.ufpe.br>
2020-07-17 13:11:53 -0300

    NavBar get data of links in API

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -11,7 +11,17 @@ import './NavBar.css';
 
 
 function NavBar(){
+    const [links, setLinks] = useState([]);
     const [show, setShow] = useState(true);
+
+    const loadLinks = async () => {
+        const res = await axios.get('http://localhost:3001/api/footer');
+        setLinks(res.data);
+    }
+    
+    useEffect(()=>{
+        loadLinks();
+    }, []);
 
     return(
         <header className="nav-bar">
@@ -38,12 +48,14 @@ function NavBar(){
                     <li><a href="#">Contatos</a></li>
                 </ul>
             </nav>
-            <div className="icons-menu">
-                <a href="#"><img src={iconTelefone} alt="Icon Telefone"/></a>
-                <a href="#"><img src={iconInstagram} alt="Icon Instagram"/></a>
-                <a href="#"><img src={iconFacebook} alt="Icon Facebook"/></a>
-                <a href="#"><img src={iconEmail} alt="Icon Email"/></a>
-            </div>
+            {links?.map(({ Instragram, Facebook, Email, Telefone })=>(
+                <div className="icons-menu">
+                    <a href={Telefone}><img src={iconTelefone} alt="Icon Telefone"/></a>
+                    <a href={Instragram}><img src={iconInstagram} alt="Icon Instagram"/></a>
+                    <a href={Facebook}><img src={iconFacebook} alt="Icon Facebook"/></a>
+                    <a href={Email}><img src={iconEmail} alt="Icon Email"/></a>
+                </div>
+            ))};
             </section>
         </header>
     );
