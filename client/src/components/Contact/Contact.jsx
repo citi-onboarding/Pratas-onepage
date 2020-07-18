a3e8bbfde9c6f912a3a437b107cdb4af0ae8fe98
Deborah Espíndola <deo2@cin.ufpe.br>
2020-07-17 22:43:15 -0300

    add frontend changes on the branch

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 import './Contact.css';
 
 function Contact(){
+    const sendEmail = async (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:3001/api/contact', {
+            name,
+            telephone,
+            email,
+            meeting,
+            message
+        });
+    }
+
+    const [name, setName] = useState('');
+    const [telephone, setTelephone] = useState('');
+    const [email, setEmail] = useState('');
+    const [meeting, setMeeting] = useState('');
+    const [message, setMessage] = useState('');
+
     return(
         <section className="contact-session" id="contact">
             <div className="contact-session-form">
-                <form action="" target="_blank">
+                <form onSubmit={sendEmail} target="_blank">
                     <h1 className="contact-title">Contato</h1>
-                    <input type="text" placeholder="Nome" className="first" id="first"/>
-                    <input type="text" placeholder="Telefone" className="first"/><br/>
-                    <input type="text" placeholder="Email"/><br/>
-                    <input type="text" placeholder="Como nos conheceu?"/><br/>
-                    <input type="textarea" placeholder="Mensagem" className="message"/><br/>
+                    <input type="text" placeholder="Nome" className="first" id="first" value={name} onChange={(event) => setName(event.target.value)} />
+                    <input type="text" placeholder="Telefone" className="first" value={telephone} onChange={(event) => setTelephone(event.target.value)} /><br/>
+                    <input type="text" placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)} /><br/>
+                    <input type="text" placeholder="Como nos conheceu?" value={meeting} onChange={(event) => setMeeting(event.target.value)} /><br/>
+                    <input type="textarea" placeholder="Mensagem" className="message" value={message} onChange={(event) => setMessage(event.target.value)} /><br/>
                     <input type="submit" className="submit" placeholder="Enviar"/>
                 </form>
             </div> 
