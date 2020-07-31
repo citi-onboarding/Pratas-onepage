c45f8902b1732bd52df2f959b4efe499ce2831bc
Daniel França <daniel.pe.15y15@gmail.com>
2020-07-31 09:39:43 -0300

    feat(update-deploy): create build

---
+++ public/precache-manifest.8968bdc3932587eb8e46ab9002a757ed.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c10fda99a511ac77771bc233819a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c48825b8eea238e2531",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "ade845821f59e0d92953",
+    "url": "/static/css/main.985fb5bd.chunk.css"
+  },
+  {
+    "revision": "3c48825b8eea238e2531",
+    "url": "/static/js/2.c7791689.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c7791689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade845821f59e0d92953",
+    "url": "/static/js/main.b150241a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a36f15e4c590928b1d91df635e77a4f1",
+    "url": "/static/media/contact-image.a36f15e4.png"
+  },
+  {
+    "revision": "c4d37f727950504511fad9fb49162ece",
+    "url": "/static/media/logo.c4d37f72.svg"
+  },
+  {
+    "revision": "98ae9144dbd97fee63dff0afef175cc2",
+    "url": "/static/media/next-arrow.98ae9144.svg"
+  },
+  {
+    "revision": "2e20f2d08d446a6ea6cdc4f60fe951af",
+    "url": "/static/media/prev-arrow.2e20f2d0.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
